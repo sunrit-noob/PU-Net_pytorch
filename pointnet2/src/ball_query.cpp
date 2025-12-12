217dc3f190f6d47e3832b8300bc75366e9390e1f
sunrit-noob <sunrit2022pers@gmail.com>
2025-12-12 17:57:11 +0530

    Updating AT_CHECK and type() which have been deprecated

--- pointnet2/src/ball_query.cpp
+++ pointnet2/src/ball_query.cpp
@@ -7,8 +7,8 @@
 
 // extern THCState *state;
 
-#define CHECK_CUDA(x) AT_CHECK(x.type().is_cuda(), #x, " must be a CUDAtensor ")
-#define CHECK_CONTIGUOUS(x) AT_CHECK(x.is_contiguous(), #x, " must be contiguous ")
+#define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x, " must be a CUDAtensor ")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x, " must be contiguous ")
 #define CHECK_INPUT(x) CHECK_CUDA(x);CHECK_CONTIGUOUS(x)
 
 int ball_query_wrapper_fast(int b, int n, int m, float radius, int nsample, 
