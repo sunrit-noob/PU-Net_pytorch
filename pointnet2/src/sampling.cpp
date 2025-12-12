649abf1df89b135166834f23e854f3c23d2daf6d
sunrit-noob <sunrit2022pers@gmail.com>
2025-12-12 17:47:05 +0530

    Removing the CUDAStream altogether from sampling

--- pointnet2/src/sampling.cpp
+++ pointnet2/src/sampling.cpp
@@ -14,8 +14,7 @@ int gather_points_wrapper_fast(int b, int c, int n, int npoints,
     const int *idx = idx_tensor.data_ptr<int>();
     float *out = out_tensor.data_ptr<float>();
 
-    cudaStream_t stream = c10::cuda::getCurrentCUDAStream();
-    gather_points_kernel_launcher_fast(b, c, n, npoints, points, idx, out, stream);
+    gather_points_kernel_launcher_fast(b, c, n, npoints, points, idx, out);
     return 1;
 }
 
@@ -27,8 +26,7 @@ int gather_points_grad_wrapper_fast(int b, int c, int n, int npoints,
     const int *idx = idx_tensor.data_ptr<int>();
     float *grad_points = grad_points_tensor.data_ptr<float>();
 
-    cudaStream_t stream = c10::cuda::getCurrentCUDAStream();
-    gather_points_grad_kernel_launcher_fast(b, c, n, npoints, grad_out, idx, grad_points, stream);
+    gather_points_grad_kernel_launcher_fast(b, c, n, npoints, grad_out, idx, grad_points);
     return 1;
 }
 
@@ -40,7 +38,6 @@ int furthest_point_sampling_wrapper(int b, int n, int m,
     float *temp = temp_tensor.data_ptr<float>();
     int *idx = idx_tensor.data_ptr<int>();
 
-    cudaStream_t stream = c10::cuda::getCurrentCUDAStream();
-    furthest_point_sampling_kernel_launcher(b, n, m, points, temp, idx, stream);
+    furthest_point_sampling_kernel_launcher(b, n, m, points, temp, idx);
     return 1;
 }
